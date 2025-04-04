3f1682f305d73f1a8f1ffb5dca9a46abbf04ecd0
Cheniev <certana.kolia@gmail.com>
2025-04-04 23:30:55 +0300

    Create Three.js

---
+++ Three.js
@@ -0,0 +1,19 @@
+/cs2-style-game
+│   index.html
+│   README.md
+│
+├───js/
+│   │   main.js
+│   │   settings.js
+│   │   player.js
+│   │   weapons.js
+│   │   enemies.js
+│
+├───models/
+│   ├───guns/
+│   └───maps/
+│
+└───textures/
+    ├───guns/
+    ├───maps/
+    └───skins/
